bd0b6109244f0ae13554913d070ef6f23cce2bcb
Rahul-codehub <rahulyadavrasalpur@zohomail.in>
2026-08-06 16:38:10 +0530

    Debug Render routes

--- server/index.mjs
+++ server/index.mjs
@@ -78,11 +78,14 @@ const server = createServer(async (req, res) => {
       return;
     }
 
-    const url = new URL(
-      req.url || "/",
-      `http://${req.headers.host || "localhost"}`
-    );
-    console.log("Parsed pathname:", url.pathname);
+   console.log("req.url =", req.url);
+console.log("host =", req.headers.host);
+
+const url = new URL(
+  `http://${req.headers.host}${req.url}`
+);
+
+console.log("pathname =", url.pathname);
 
     const pathParts = url.pathname
       .split("/")
