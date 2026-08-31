7dae2a3d6a4082227e1e61cd467bad0a3820e25b
Rahul-codehub <rahulyadavrasalpur@zohomail.in>
2026-09-01 00:10:42 +0530

    Chat Window Fixed

--- src/components/chatbot/ChatWindow.tsx
+++ src/components/chatbot/ChatWindow.tsx
@@ -2,7 +2,6 @@ import ChatHeader from "./ChatHeader";
 import ChatMessages from "./ChatMessages";
 import ChatInput from "./ChatInput";
 
-
 interface ChatWindowProps {
   onClose: () => void;
 }
@@ -13,44 +12,37 @@ export default function ChatWindow({
   return (
     <div
       className="
-    flex
-    h-[760px]
-    w-[500px]
-    flex-col
-
-    overflow-hidden
-
-    rounded-[28px]
-
-    border
-    border-slate-200
-
-    bg-white
-
-    shadow-2xl
-  "
+        flex
+        h-[600px]
+        max-h-[calc(100vh-120px)]
+        w-[420px]
+        max-w-[calc(100vw-32px)]
+        flex-col
+        overflow-hidden
+        rounded-[28px]
+        border
+        border-slate-200
+        bg-white
+        shadow-2xl
+      "
     >
       {/* Header */}
-
-      <ChatHeader
-        onClose={onClose}
-      />
+      <ChatHeader onClose={onClose} />
 
       {/* Body */}
-
       <div
         className="
-    flex
-    flex-1
-    overflow-hidden
-    bg-slate-50
-  "
+          flex
+          min-h-0
+          flex-1
+          overflow-hidden
+          bg-slate-50
+        "
       >
         <ChatMessages />
       </div>
 
       {/* Input */}
-
       <ChatInput />
     </div>
   );
